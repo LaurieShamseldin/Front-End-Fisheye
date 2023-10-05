b45b04494db1c53a79ea1efb36031653084bf780
Laurie Shamseldin <laurie.musson@gmail.com>
2023-10-05 15:45:13 +0200

    add data photographer

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,33 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const photographerInfo = document.createElement( 'div' );
+        photographerInfo.classList.add('card-photographer');
+
+        const local = document.createElement ( 'span' );
+        local.classList.add('card-city');
+        local.textContent = `${city}, ${country}`;
+
+        const photographerTagline = document.createElement( 'p' );
+        photographerTagline.classList.add('card-tagline');
+        photographerTagline.textContent = tagline;
+
+        const photographerPackage = document.createElement( 'p' );
+        photographerPackage.classList.add('card-package');
+        photographerPackage.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(photographerInfo);
+        photographerInfo.appendChild(local);
+        photographerInfo.appendChild(photographerTagline);
+        photographerInfo.appendChild(photographerPackage);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
+
+// "tagline": "Toujours à la recherche de LA photo",
+// "price": 300,
